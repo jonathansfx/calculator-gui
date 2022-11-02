34a3fdbdbb31eab683712c9f00902efe7d46d828
jonathan <jonathanluo22@gmail.com>
2022-11-01 19:43:45 -0600

    asdfsadfsadfsadf

--- main.js
+++ main.js
@@ -3,6 +3,22 @@
 //ADDEVENTLISTENER
 document.getElementById("btn").addEventListener("click", btnClicked);
 
+function btnClicked() {
 //INPUT
+let A = +document.getElementById("a-in").value;
+let B = +document.getElementById("b-in").value;
+let C = +document.getElementById("c-in").value;
+let D = +document.getElementById("d-in").value;
+let E = +document.getEllementById("e-in").value;
+
+
+
+//PROCESS
+let average = (A + B + C + D + E) / 5
+
 
 //OUTPUT
+let output = +document.getElementById(average)
+
+
+}
