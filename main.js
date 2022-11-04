e92fcb76276f94dfef76d27c8a5c04e2c2352f02
Jonathan Luo <97814319+zombie0000@users.noreply.github.com>
2022-11-04 14:41:12 -0600

    asdf

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ function btnClicked() {
   let B = +document.getElementById("b-in").value;
   let C = +document.getElementById("c-in").value;
   let D = +document.getElementById("d-in").value;
-  let E = +document.getEllementById("e-in").value;
+  let E = +document.getElementById("e-in").value;
 
   //PROCESS
   let average = (A + B + C + D + E) / 5;
 
   //OUTPUT
-  document.getElementById("output").innerHTML = average;
+  document.getElementById("output").innerHTML = average.toFixed(0);
 }
